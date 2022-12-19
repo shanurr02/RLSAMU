dfc748e33f6cf1b00fb0fcaa6926960267c6fc49
Mohd Tanveer Hasan <mtanveerhasan2001@gmail.com>
2022-12-19 21:07:06 +0530

    nav-menu, container-btn, color

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("OHAYO")
+function btnaction() {
+    var menu = document.getElementById("menu-btn");
+    var nav = document.getElementById("nav");
+    menu.classList.toggle("hide");
+    nav.classList.toggle("show");
+}

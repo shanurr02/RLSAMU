2509f21fd7c9de00efafb8fe3fb16e625fb79527
Mohd Tanveer Hasan <mtanveerhasan2001@gmail.com>
2023-01-11 19:47:21 +0530

    prose page and new nav panel

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ console.log("OHiYO")
 function btnaction() {
     var menu = document.getElementById("menu-btn");
     var nav = document.getElementById("nav");
-    menu.classList.toggle("hide");
+    // menu.classList.toggle("hide");
     nav.classList.toggle("show");
+    var side_nav = document.getElementById("side-nav");
+    side_nav.classList.remove("show");
+}
+function linkaction() {
+    var side_nav = document.getElementById("side-nav");
+    side_nav.classList.toggle("show");
 }
